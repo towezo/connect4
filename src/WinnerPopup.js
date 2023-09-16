715fbf54fe7cd9d1fc11a50ecbf2411675a8c543
towezo <thiespooch@icloud.com>
2023-09-16 09:32:38 +0200

    changes

--- src/WinnerPopup.js
+++ src/WinnerPopup.js
@@ -9,7 +9,6 @@ function WinnerPopup({ winner, player1Name, player2Name, onNewGame, onQuitGame }
       <div className="flex justify-center text-xl font-semibold">{winner === 'red' ? player1Name : player2Name} wins!</div>
       <div className='flex justify-center'>
     </div>
-
       <div className="flex justify-center">
         <button
           className="flex-initial w-32 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-4 rounded"
