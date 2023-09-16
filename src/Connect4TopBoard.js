59e330cf958c95509e7352cd9517f082db14ffac
towezo <thiespooch@icloud.com>
2023-09-16 11:26:14 +0200

    ggg

--- src/Connect4TopBoard.js
+++ src/Connect4TopBoard.js
@@ -2,12 +2,9 @@ import React from "react";
 import Connect4winnerboard from './Connect4winnerboard';
 
 function Connect4TopBoard({ player1Name, player2Name, player1Wins, player2Wins, winners, winner }) {
-
     return(
     <div className='  h-1/3 '> 
-    {/* Winnerboard*/}
     <Connect4winnerboard winners={winners} winner={winner} player1Name={player1Name} player2Name={player2Name} player1Wins={player1Wins} player2Wins={player2Wins} />
-  
 </div>
 
 );
